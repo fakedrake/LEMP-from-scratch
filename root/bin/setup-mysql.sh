da1ad2a29a891e16de1b110404216b7e9c2f5864
Chris Perivolaropoulos <cperivol@csail.mit.edu>
2015-05-10 18:10:38 +0300

    Sql setup script

---
+++ root/bin/setup-mysql.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+root_user=root
+root_pass=password
+
+${root_dir}/scripts/mysql_install_db --basedir=${root_dir} --user=${root_user}
+${root_dir}/bin/mysqladmin -u ${root_user} password "${root_pass}"
+
+# Mediawiki stuff
+echo "Creating mediawiki database"
+echo "create database wikidb; grant index, create, select, insert, update, delete, drop, alter, lock tables on wikidb.* to '${root_user}'@'localhost' identified by '${root_pass}'" | ${root_dir}/bin/mysql -u ${root_user} -p ${root_pass}
